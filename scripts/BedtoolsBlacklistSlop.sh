85317a903282d87f2cb05d42e8a9a04f1a8c290e
Cazares, Tareian <tacazares@gmail.com>
2021-11-17 11:09:01 -0500

    Create BedtoolsBlacklistSlop.sh
    This script will remove blacklisted regions from a BED file, slop it by a specific number of bases, sort the file, then compress the file with pigz.

---
+++ scripts/BedtoolsBlacklistSlop.sh
@@ -0,0 +1,29 @@
+########################################################################
+# Use bedtools to remove blacklisted regions and slop a BED file
+########################################################################
+
+###### INPUTS ######
+bed_file=${1}
+output_dir=${2}
+blacklist_bed=${3}
+chrom_sizes=${4}
+slop_size=${5}
+
+###### Parameters ######
+
+# Set up output file names
+slop_file=`basename ${bed_file} .bed.gz`_slop${slop_size}.bed.gz
+
+echo "Making directory " ${output_dir}
+mkdir -p ${output_dir}
+cd ${output_dir}
+
+###### WORKFLOW ######
+echo "BED file: " ${bed_file} 
+echo "Output directory: " ${output_dir} 
+echo "Output File: " ${slop_size}
+echo "Blacklist BED file: " ${blacklist_bed} 
+echo "Chromosome sizes file: " ${chrom_sizes} 
+echo "Slop size: " ${slop_size}
+
+bedtools intersect -a ${bed_file} -b ${blacklist_bed} -v | bedtools slop -i - -g hg38.chrom.sizes -b ${slop_size} | bedtools sort | pigz > ${slop_file}
